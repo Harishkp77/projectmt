9d0080ce2b0dede7997121d3c00082b4bc43774d
Harishkp77 <137972179+Harishkp77@users.noreply.github.com>
2023-07-27 12:16:34 +0530

    added text box validatee

--- src/components/Loginpage.js
+++ src/components/Loginpage.js
@@ -3,12 +3,18 @@ import React, { useState } from "react";
 const Loginpage = () => {
   const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const checklogin = () => {
-    if (username === "admin" && password === "1111") {
+    if (username.trim() === "" || password === "") {
+      setErrorMessage("Please fill in both username and password.");
+    } else if (username === "admin" && password === "1111") {
       alert("login successful");
+      setErrorMessage("");
+      setusername("");
+      setpassword(""); 
     } else {
-      alert("invaliduser");
+      setErrorMessage("Invalid username or password.");
     }
   };
 
@@ -17,25 +23,25 @@ const Loginpage = () => {
       <div className="cover-card">
         <h1 class="cover-title">Login</h1>
         <form action="" className="cover-group">
-            <label className="cover-label">Username</label>
-            <input
-              type="text"
-              placeholder="Enter username"
-              className="cover-label"
-              onChange={(e) => setusername(e.target.value)}
-            />
-            <label className="cover-label">Password</label>
-            <input
-              type="Password"
-              placeholder="Enter password"
-              className="cover-label"
-              onChange={(e) => setpassword(e.target.value)}
-            />
+          <label className="cover-label">Username</label>
+          <input
+            type="text"
+            placeholder="Enter username"
+            className="cover-label"
+            onChange={(e) => setusername(e.target.value)}
+          />
+          <label className="cover-label">Password</label>
+          <input
+            type="Password"
+            placeholder="Enter password"
+            className="cover-label"
+            onChange={(e) => setpassword(e.target.value)}
+          />
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-            <button onClick={checklogin} class="cover-button">
-              Login
-            </button>
-      
+          <button onClick={checklogin} class="cover-button">
+            Login
+          </button>
         </form>
       </div>
     </section>
